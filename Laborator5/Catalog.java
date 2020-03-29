5a954a747afe16fdbf1574cb8475b046813b6052
Alexandra Olariu <61508600+alexandraolariu@users.noreply.github.com>
2020-03-29 19:10:35 +0300

    Create Catalog.java

---
+++ Laborator5/Catalog.java
@@ -0,0 +1,51 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog implements Serializable {
+    private String name;
+    private String path;
+    private List<Document> documents = new ArrayList<>();
+
+    //…
+    public void add(Document doc) {
+        documents.add(doc);
+    }
+    public Document findById(String id) {
+        return documents.stream()
+                .filter(d -> d.getId().equals(id)).findFirst().orElse(null);
+    }
+    public Catalog() {
+
+    }
+
+    public Catalog(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return "Catalog{" +
+                "name='" + name + '\'' +
+                ", path='" + path + '\'' +
+                ", documents=" + documents +
+                '}';
+    }
+}
