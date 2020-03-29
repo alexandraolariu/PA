d43fb5a56c1d0b327475ab42eaad1a433b6a4c15
Alexandra Olariu <61508600+alexandraolariu@users.noreply.github.com>
2020-03-29 19:07:46 +0300

    Create Resident.java

---
+++ Laborator4/Resident.java
@@ -0,0 +1,22 @@
+public class Resident {
+    private String name;
+
+    public Resident(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Resident{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
