a84d68b691000d6af0ad60e42e3347edd88b8e63
Alexandra Olariu <61508600+alexandraolariu@users.noreply.github.com>
2020-04-04 15:27:35 +0300

    Update Main.java

--- Lab7/Main.java
+++ Lab7/Main.java
@@ -1,18 +1,5 @@
-
-public class Game {
-    public void main(){
-        Board board=new Board(20,6);
-        Runnable Jucator1 = new Player("Alex",board);
-        Runnable Jucator2 = new Player("Mihai",board);
-        Runnable Jucator3 = new Player("Ion",board);
-        Runnable Jucator4 = new Player("Andrei",board);
-        Runnable Jucator5 = new Player("Andreea",board);
-        Runnable Jucator6 = new Player("Mihaela",board);
-        new Thread(Jucator1).start();
-        new Thread(Jucator2).start();
-        new Thread(Jucator3).start();
-        new Thread(Jucator4).start();
-        new Thread(Jucator5).start();
-        new Thread(Jucator6).start();
-}
+public class Main {
+    public static void main(String[] args) {
+       new Game().main();
+    }
 }
